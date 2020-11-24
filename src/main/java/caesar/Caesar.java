9255dab79135b7aa5b89eb35b44a107f0c971f83
DaniKard <kajothada@gmail.com>
2020-11-24 12:54:55 +0200

    add caesarcipher method in casearclass

--- src/main/java/caesar/Caesar.java
+++ src/main/java/caesar/Caesar.java
@@ -8,7 +8,33 @@ public class Caesar  {
             shift = (shift % 26) + 26;
         }
         String cipherText = "";
+        int length = plainText.length();
+        for (int i= 0; i<length; i++){
+            char ch= plainText.charAt(i);
+            if (Character.isLetter(ch)) {
+                if(Character.isLowerCase(ch)){
+                    char c = (char) (ch+shift);
+                    if(c>'z'){
+                        cipherText +=(char)(ch -(26-shift));
+                    }
+                    else {
+                        cipherText += c;
+                    }
+                }
+            }
+                else if(Character.isUpperCase(ch)){
+                char c = (char) (ch+shift);
+                if(c>'Z'){
+                    cipherText +=(char)(ch -(26-shift));
+                }
+                else {
+                    cipherText += c;
+                }
 
+                }
+            else{ cipherText +=ch;
+            }
+        }
         return cipherText;
     }
 public static void main (String[] args){
