d4f936871ddbb7c2ecdd734cfc24318856c3af3f
DaniKard <kajothada@gmail.com>
2020-11-24 13:53:32 +0200

    add encrpt and decrypt methods

--- src/main/java/caesar/Caesar.java
+++ src/main/java/caesar/Caesar.java
@@ -37,7 +37,49 @@ public class Caesar  {
         }
         return cipherText;
     }
+    public static String decrypt(String plainText,int shift) {
+        if (shift > 26) {
+            shift = shift % 26;
+        } else if (shift < 0) {
+            shift = (shift % 26) + 26;
+        }
+        String cipherText = "";
+        int length = plainText.length();
+        for (int i= 0; i<length; i++){
+            char ch= plainText.charAt(i);
+            if (Character.isLetter(ch)) {
+                if(Character.isLowerCase(ch)){
+                    char c = (char) (ch-shift);
+                    if(c<'a'){
+                        cipherText +=(char)(ch +(26-shift));
+                    }
+                    else {
+                        cipherText += c;
+                    }
+                }
+            }
+            else if(Character.isUpperCase(ch)){
+                char c = (char) (ch-shift);
+                if(c<'A'){
+                    cipherText +=(char)(ch +(26-shift));
+                }
+                else {
+                    cipherText += c;
+                }
+
+            }
+            else{ cipherText +=ch;
+            }
+        }
+        return cipherText;
+    }
 public static void main (String[] args){
+        String text ="this is a massage";
+        String cipher = encrypt(text,2);
+        System.out.println(cipher);
+        String decrypt = decrypt(cipher,2);
+        System.out.println(decrypt);
+
         }
 }
 
