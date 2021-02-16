caba675040c7eaf3787017ebbe13965038bec7c7
karambizi <kajothada@gnail.com>
2021-02-16 16:01:39 +0200

    impliment successful test

--- src/main/java/caesar/CaesarTest.java
+++ src/main/java/caesar/CaesarTest.java
@@ -1,10 +1,21 @@
 package caesar;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CaesarTest {
-
-    @org.junit.jupiter.api.Test
-    void encrypt() {
+public class CaesarTest {
+    private Caesar caesar = new Caesar();
+    @Test
+    public void ignoreEmptyStrings(){
+        assertEquals("", caesar.encrypt("", 3));
     }
+    public void readSentence(){
+        String sentence ="hello world";
+        assertEquals(sentence, caesar.encrypt(sentence, 2));
+
+    }
+
+
 }
+
+
